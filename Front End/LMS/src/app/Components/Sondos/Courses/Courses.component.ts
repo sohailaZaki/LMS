5740bfa0bcd909696388c12f51c64b86ceaf36bc
sohaila <sohailazaki@gmail.com>
2024-12-09 07:53:28 +0200

    Instructor Dashboard Creation

---
+++ Front End/LMS/src/app/Components/Sondos/Courses/Courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Courses',
+  templateUrl: './Courses.component.html',
+  styleUrls: ['./Courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
