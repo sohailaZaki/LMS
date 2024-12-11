82fa6797771b9e51769a92be5f31493a91004d38
sohaila <sohailazaki@gmail.com>
2024-12-11 18:55:39 +0200

    fromated layout

---
+++ Front End/LMS/src/app/Components/Instructor/Sondos/Grades/Grades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Grades',
+  templateUrl: './Grades.component.html',
+  styleUrls: ['./Grades.component.css']
+})
+export class GradesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
