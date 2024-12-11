3d40d31621215279d6e3fd56ce7c917ca5722434
sohaila <sohailazaki@gmail.com>
2024-12-11 15:10:10 +0200

    layout updates

--- Front End/LMS/src/app/app.module.ts
+++ Front End/LMS/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { InstructorDashBoardComponent } from './Components/Sohaila/instructorDashBoard/instructorDashBoard.component';
-import { Router, RouterLink, RouterModule, Routes } from '@angular/router';
+import { Router, RouterLink, RouterModule, Routes,RouterOutlet } from '@angular/router';
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzListComponent, NzListItemComponent } from 'ng-zorro-antd/list';
-import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonComponent ,NzButtonModule} from 'ng-zorro-antd/button';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
@@ -21,9 +24,17 @@ import { provideHttpClient } from '@angular/common/http';
 import { SideBarComponent } from './Components/Sondos/SideBar/SideBar.component';
 import { CoursesComponent } from './Components/Sondos/Courses/Courses.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-const routes: Routes=[{
-  path:'InstructorDashBoard',component:InstructorDashBoardComponent},
-  {path:'Instructor/Courses',component:CoursesComponent }
+import { CourseViewComponent } from './Components/Sohaila/courseView/courseView.component';
+import { LayoutComponent } from './Components/Layout/Layout.component';
+import { NzBackTopModule } from 'ng-zorro-antd/back-top';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+const routes: Routes=[
+
+  {path:'',component:LayoutComponent,children:[
+{path:'InstructorDashBoard',component:InstructorDashBoardComponent},
+{path:'Instructor/Courses',component:CoursesComponent },
+{path:'Instructor/Courses/:id',component:CoursesComponent },
+  ] }
 
 ]
 registerLocaleData(en);
@@ -33,12 +44,14 @@ registerLocaleData(en);
     InstructorDashBoardComponent,
     SideBarComponent,
     CoursesComponent,
+    CourseViewComponent,LayoutComponent
   ],
   imports: [
     BrowserModule,
     MatCardModule, IconsProviderModule, NzLayoutModule, NzMenuModule, FormsModule,NzCardModule,MatListModule,NzListItemComponent,NzListComponent,NzButtonComponent,
-    RouterModule.forRoot(routes),
-    NgbModule
+    NgbModule,NzDropDownModule,NzIconModule,NzButtonModule,MatIcon,
+    RouterOutlet,NzBackTopModule,ScrollingModule,
+    RouterModule.forRoot(routes)
   ],
 
   providers: [
