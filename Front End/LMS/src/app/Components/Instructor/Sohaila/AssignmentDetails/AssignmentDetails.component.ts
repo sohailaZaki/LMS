730691c0e965c84ad33239952a1079b2129028db
sohailaZaki <sohailazakifcis@gmail.com>
2024-12-13 11:09:32 +0200

    adding service

---
+++ Front End/LMS/src/app/Components/Instructor/Sohaila/AssignmentDetails/AssignmentDetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-AssignmentDetails',
+  templateUrl: './AssignmentDetails.component.html',
+  styleUrls: ['./AssignmentDetails.component.css']
+})
+export class AssignmentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
