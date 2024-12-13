3809f1c21387d8bb3cd65a3e6cb5cb81346efa23
sohailaZaki <143770585+sohailaZaki@users.noreply.github.com>
2024-12-13 11:14:14 +0200

    Sohaila (#19)
    * edit errors
    
    * edit errors
    
    * link register and login
    
    * adding service

--- Front End/LMS/src/app/Components/Instructor/Sondos/Grades/Grades.component.ts
+++ Front End/LMS/src/app/Components/Instructor/Sondos/Grades/Grades.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Grades.component.css']
 })
 export class GradesComponent implements OnInit {
+  students = [
+    { name: 'Sohaila', grade: 100 },
+    { name: 'Sondos', grade: 100 },
+    { name: 'Nada', grade: 100 },
+    { name: 'Aya', grade: 100 },
+    { name: 'Sara', grade: 100 }
+  ];
 
+  editingIndex: number | null = null;
+
+  editGrade(index: number) {
+    this.editingIndex = index;
+  }
+
+  saveGrade(index: number) {
+    this.editingIndex = null;
+    console.log('Updated Grade:', this.students[index]);
+  }
   constructor() { }
 
   ngOnInit() {
