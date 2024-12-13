c9d4ba61a58b3cf6b7b48f4a0b20940265349ccd
sohailaZaki <143770585+sohailaZaki@users.noreply.github.com>
2024-12-13 02:25:42 +0200

    signin and signup (#10)
    * signin and signup
    
    * Confilcts Solved
    
    ---------
    
    Co-authored-by: RahmaAlaa <alaarahma779@gmail.com>

--- Front End/LMS/src/app/Components/Layout/Layout.component.ts
+++ Front End/LMS/src/app/Components/Layout/Layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-Layout',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Layout.component.css']
 })
 export class LayoutComponent implements OnInit {
+  showSidebar:boolean=true;
+  /**
+   *
+   */
+  constructor(private route:Router) {
+    if(route.url.includes('Login' || route.url.includes('signup'))){
+      this.showSidebar=false;
+    }
+    else{
+      this.showSidebar=true;
+    }
+  }
 
-  constructor() { }
 
   ngOnInit() {
   }
