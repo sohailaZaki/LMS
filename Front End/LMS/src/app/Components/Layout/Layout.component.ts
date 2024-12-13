b391ab9a5d894b0c779cba3c903f7eecc106feeb
sohailaZaki <sohailazakifcis@gmail.com>
2024-12-13 08:28:04 +0200

    link register and login

--- Front End/LMS/src/app/Components/Layout/Layout.component.ts
+++ Front End/LMS/src/app/Components/Layout/Layout.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-Layout',
   templateUrl: './Layout.component.html',
-  styleUrls: ['./Layout.component.css']
+  styleUrls: ['./Layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
+  currentRoute: string = '';
+  showSidebar: boolean = true;
+  /**
+   *
+   */
+  constructor(private route: Router) {
+    this.currentRoute = route.url;
 
-  constructor() { }
+this.route.events.pipe(filter(event=>event instanceof NavigationEnd)).subscribe((event:any)=>{
+  this.currentRoute=event.url;
+  this.updateSidebarVisability();
+})
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.currentRoute = this.route.url;
+    this.updateSidebarVisability();
+  }
+  updateSidebarVisability() {
+    if (
+      this.currentRoute.includes('Login') ||
+      this.currentRoute.includes('signup')
+    ) {
+      this.showSidebar = false;
+    } else {
+      this.showSidebar = true;
+    }
+  }
 }
---
+++ Front End/LMS/src/app/Components/Student/Nada/course-materials/course-materials.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-course-materials',
+  standalone: false,
+  templateUrl: './course-materials.component.html',
+  styleUrls: ['./course-materials.component.css']
+})
+export class CourseMaterialsComponent {
+  selectedLecture: any; // سيتم تخزين المحاضرة المختارة هنا
+  selectedLabs: any = null;
+
+  courses = [
+    {
+      id: '1',
+      title: 'Angular Course',
+      description: 'Detailed description about Angular course',
+      image: 'assets/channels4_profile.jpg',
+      progress: 5,
+      lectures: [
+        { name: "Angular Basics", pdfUrl: "assets/A01.pdf" },
+        { name: "Angular Directives", pdfUrl: "assets/A01.pdf" },
+        { name: "Angular Components", pdfUrl: "assets/A01.pdf" }
+      ],
+      labs: [
+        { name: "Node Basics", pdfUrl: "assets/A01.pdf" },
+        { name: "Node.js API", pdfUrl: "assets/A01.pdf" }
+      ],
+      assessment: [
+        { name: "Node Basics", pdfUrl: "assets/A01.pdf" },
+        { name: "Node.js API", pdfUrl: "assets/A01.pdf" }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Node.js',
+      description: 'Detailed description about Node course',
+      image: 'assets/channels4_profile.jpg',
+      progress: 75,
+      lectures: [
+        { name: "Node Basics", pdfUrl: "assets/A01.pdf" },
+        { name: "Node.js API", pdfUrl: "assets/A01.pdf" }
+      ],
+      labs: [
+        { name: "Node Basics", pdfUrl: "assets/A01.pdf" },
+        { name: "Node.js API", pdfUrl: "assets/A01.pdf" }
+      ],
+      assessment: [
+        { name: "Node Basics", pdfUrl: "assets/A01.pdf" },
+        { name: "Node.js API", pdfUrl: "assets/A01.pdf" }
+      ]
+
+    },
+    // باقي الكورسات هنا
+  ];
+
+  constructor(private router: Router) {}
+
+
+  navigateToCourse(course: any) {
+    this.router.navigate(['/dashboard/course-detail', course.id], { state: { courseDetails: course } });
+  }
+}
