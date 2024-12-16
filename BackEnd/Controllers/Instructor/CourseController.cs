0e5227d908aab6db06f21b194d975c4e0091dadc
sohailaZaki <sohailazakifcis@gmail.com>
2024-12-16 04:24:07 +0200

    BackEnd Changes

---
+++ BackEnd/Controllers/Instructor/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using LMS.Data.Models;
+using LMS.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Controllers.Instructor
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        public CourseController( AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> getCourses()
+        {
+            var courses = await _db.Courses.ToListAsync();
+            return Ok(courses);
+        }
+        
+        private readonly AppDbContext _db;
+    }
+}
